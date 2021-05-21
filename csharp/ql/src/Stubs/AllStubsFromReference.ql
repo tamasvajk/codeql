c522a9613e69ced5dcaaa354bb495bbefe6ca076
Tamas Vajk <tamasvajk@github.com>
2021-05-21 15:48:05 +0200

    WIP: Added asp.net core, and .net core stubs, modified the test to reference it

--- csharp/ql/src/Stubs/AllStubsFromReference.ql
+++ csharp/ql/src/Stubs/AllStubsFromReference.ql
@@ -10,11 +10,10 @@ class AllExternalPublicDeclarations extends GeneratedDeclaration {
   AllExternalPublicDeclarations() { this.fromLibrary() }
 }
 
-/** All framework assemblies. */
-class NonTargetAssembly extends ExcludedAssembly {
-  NonTargetAssembly() { this.getFile().getAbsolutePath().matches("%Microsoft.NETCore.App.Ref%") }
-}
-
+// /** All framework assemblies. */
+// class NonTargetAssembly extends ExcludedAssembly {
+//   NonTargetAssembly() { this.getFile().getAbsolutePath().matches("%Microsoft.NETCore.App.Ref%") }
+// }
 from Assembly a
 select a.getFullName(), a.getName(), a.getVersion().toString(), a.getFile().getAbsolutePath(),
   generatedCode(a)
--- csharp/ql/test/query-tests/Stubs/References/Test.cs
+++ csharp/ql/test/query-tests/Stubs/References/Test.cs
@@ -1,41 +1,6 @@
-// semmle-extractor-options: --load-sources-from-project:../../../resources/stubs/Swashbuckle.AspNetCore.Swagger/6.1.4/Swashbuckle.AspNetCore.Swagger.csproj --framework:/usr/local/share/dotnet/packs/Microsoft.NETCore.App.Ref/5.0.0/ref/net5.0/
+// semmle-extractor-options: --load-sources-from-project:../../../resources/stubs/frameworks/Microsoft.AspNetCore.App/Microsoft.AspNetCore.App.csproj
 // semmle-extractor-options: /nostdlib
-// semmle-extractor-options: /r:System.Console.dll
-// semmle-extractor-options: /r:System.Net.Http.dll
-// semmle-extractor-options: /r:System.Diagnostics.TraceSource.dll
-// semmle-extractor-options: /r:System.ComponentModel.dll
-// semmle-extractor-options: /r:System.IO.FileSystem.dll
-// semmle-extractor-options: /r:System.Net.Primitives.dll
-// semmle-extractor-options: /r:System.Text.RegularExpressions.dll
-// semmle-extractor-options: /r:System.Security.Cryptography.X509Certificates.dll
-// semmle-extractor-options: /r:System.ComponentModel.EventBasedAsync.dll
-// semmle-extractor-options: /r:System.Text.Json.dll
-// semmle-extractor-options: /r:System.Security.Cryptography.Primitives.dll
-// semmle-extractor-options: /r:System.Security.Claims.dll
-// semmle-extractor-options: /r:System.Net.WebSockets.dll
-// semmle-extractor-options: /r:System.Linq.Expressions.dll
-// semmle-extractor-options: /r:System.Security.Cryptography.Algorithms.dll
-// semmle-extractor-options: /r:System.Collections.NonGeneric.dll
-// semmle-extractor-options: /r:System.ComponentModel.Primitives.dll
-// semmle-extractor-options: /r:System.Security.Cryptography.Csp.dll
-// semmle-extractor-options: /r:System.IO.Compression.dll
-// semmle-extractor-options: /r:Microsoft.Win32.Primitives.dll
-// semmle-extractor-options: /r:System.Collections.dll
-// semmle-extractor-options: /r:System.IO.FileSystem.Watcher.dll
-// semmle-extractor-options: /r:System.Text.Encodings.Web.dll
-// semmle-extractor-options: /r:System.Xml.ReaderWriter.dll
-// semmle-extractor-options: /r:System.Diagnostics.DiagnosticSource.dll
-// semmle-extractor-options: /r:System.Xml.XDocument.dll
-// semmle-extractor-options: /r:System.Memory.dll
-// semmle-extractor-options: /r:System.ComponentModel.Annotations.dll
-// semmle-extractor-options: /r:System.Data.Common.dll
-// semmle-extractor-options: /r:System.Runtime.Serialization.Xml.dll
-// semmle-extractor-options: /r:System.Net.Security.dll
-// semmle-extractor-options: /r:netstandard.dll
-// semmle-extractor-options: /r:System.Diagnostics.Tracing.dll
-// semmle-extractor-options: /r:System.Runtime.InteropServices.dll
-// semmle-extractor-options: /r:System.Xml.XmlSerializer.dll
-// semmle-extractor-options: /r:System.Threading.dll
+// semmle-extractor-options: /noconfig
 
 namespace Test
 {
