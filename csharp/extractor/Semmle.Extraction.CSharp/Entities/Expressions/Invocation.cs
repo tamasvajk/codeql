33faea7437082fdc11f81cb3aa6dbc8aec286324
Tamas Vajk <tamasvajk@github.com>
2023-12-01 14:06:37 +0100

    Remove error reporting when no member name was found for an invocation

--- csharp/extractor/Semmle.Extraction.CSharp/Entities/Expressions/Invocation.cs
+++ csharp/extractor/Semmle.Extraction.CSharp/Entities/Expressions/Invocation.cs
@@ -87,9 +87,9 @@ namespace Semmle.Extraction.CSharp.Entities.Expressions
             else
             {
                 if (memberName is not null)
+                {
                     trapFile.invocation_member_name(this, memberName);
-                else
-                    Context.ModelError(Syntax, "Unable to get name for call.");
+                }
             }
 
             PopulateArguments(trapFile, Syntax.ArgumentList, child);
